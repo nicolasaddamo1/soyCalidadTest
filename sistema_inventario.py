68975752c48f25c5954059f32a22ec6e0cbdbd24
Nico <nicolasaddamo1@gmail.com>
2025-04-15 14:29:45 -0300

    feat: Added show products function

--- sistema_inventario.py
+++ sistema_inventario.py
@@ -1,3 +1,13 @@
 Productos = {1: 'Pantalones', 2: 'Camisas', 3: 'Corbatas', 4: 'Casacas'}
 Precios = {1: 200.00, 2: 120.00, 3: 50.00, 4: 350.00}
 Stock = {1: 50, 2: 45, 3: 30, 4: 15}
+
+def mostrar_productos():
+    print("=" * 20)
+    print("Lista de Productos:")
+    print("=" * 20)
+    for codigo in Productos:
+        print(f"{codigo} {Productos[codigo]} {Precios[codigo]} {Stock[codigo]}")
+    print("=" * 20)
+
+mostrar_productos()
